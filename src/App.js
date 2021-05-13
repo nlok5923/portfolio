bc45f02ccd6c90bd3e2f828cab3474e33d2c2c6c
nlok5923 <nlokhande5923@gmail.com>
2021-05-13 17:08:59 +0530

    added background

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Header from "./components/Header/index"
 const App = () => {
   return(
     <div className="page">
+      {/* <div className="stars"></div>
+      <div className="twinkling"></div> */}
       <Router>
       <Header />
         <Switch>
