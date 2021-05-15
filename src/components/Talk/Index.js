7ed172afb12e99e603da1ed55bbe08a7c4cc298b
nlok5923 <nlokhande5923@gmail.com>
2021-05-15 15:22:25 +0530

    dummy links -> actual links in about page

--- src/components/Talk/Index.js
+++ src/components/Talk/Index.js
@@ -2,6 +2,7 @@ import { React, useEffect, useState } from "react"
 import './Talk.scss'
 
 const Talk = (props) => {
+    // const [ishover, setIshover] = useState(false);
     const [message, setMessage] = useState("");
     useEffect(() => {
         var i = 0;
@@ -22,7 +23,8 @@ const Talk = (props) => {
 
     return (
         <div>
-           <div className="speak__container">
+           <div className="speak__container" >
+           {/* <img src="images/cross.svg" className={`speak__container__img ${ishover ? "show" :"no-show"}`} alt="nitanshu"  className="cross" /> */}
                <div className="talk__container">
                    <p className="bubble speech">{message}</p>
                    <img src="images/astro.jpg" className="speak__container__img" alt="nitanshu" />
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -2,8 +2,14 @@ import { React } from "react"
 import "./About.scss"
 import Talk from "../../components/Talk/Index"
 import * as conversation from "../../content/conversation.json";
+import * as aboutme from "../../content/about.json";
 
 const About = () => {
+
+    const socialHandles = ["fa fa-facebook", "fa fa-instagram", "fa fa-linkedin"];
+    const codingHandles = ["codechef", "codeforces", "github"];
+    var i = -1;
+    var j = -1;
 
     return(
         <div>
@@ -11,41 +17,26 @@ const About = () => {
         <div className="about">
             <div className="about__container">
                 <div className="coding__handles">
-                <a
-                href="https://www.facebook.com/nitanshu.lokhande.9/"
-                class="fa fa-facebook"
-                >
-                </a>
-                 <a href="https://www.instagram.com/nit_lok/" class="fa fa-instagram">
-                </a>
-                <a 
-                href="https://www.linkedin.com/in/nitanshu-lokhande-5b60691a0/"
-                class="fa fa-linkedin"
-                >
-                </a>
+                    {aboutme.about[2].coding__links.map((data, index) => {
+                        j++;
+                        let name = "images/" + codingHandles[j] + '.svg';
+                        return(
+                            <a href={data}><img src={name} className="handle__size" alt='coding handle' /></a>
+                            )
+                    })}
                 </div>
                 <img className="about__container__img" src="images/nitanshu.jpg" alt="me" />
                 <div className="about__container__info" >
-                    <h1>Hi 👋 this is Nitanshu Lokhande</h1>
+                    <h1>{aboutme.about[0].heading}</h1>
                     <div className="info__para">
-                        <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
+                        <p>{aboutme.about[0].para}</p>
                     </div>
                 </div>
                 <div className="social__handles">
-                <a
-                href="https://www.facebook.com/nitanshu.lokhande.9/"
-                className="fa fa-facebook"
-                >
-                </a>
-                 <a href="https://www.instagram.com/nit_lok/" className="fa fa-instagram">
-                </a>
-                <a 
-                href="https://www.linkedin.com/in/nitanshu-lokhande-5b60691a0/"
-                className ="fa fa-linkedin"
-                >
-                </a>
+                {aboutme.about[1].social__links.map((data, index) =>{ 
+                 i++;
+                 return (<a href={data} className={socialHandles[i]} >&emsp;</a>)
+                 })}
                 </div>
             </div>
             {/* {!load ? <Flying />: */}
