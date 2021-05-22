180cb0ac2d697d78b596b2863f41b665243fc53d
nlok5923 <nlokhande5923@gmail.com>
2021-05-22 16:01:06 +0530

    added tech stack logo to tools page

--- src/pages/Tools/index.js
+++ src/pages/Tools/index.js
@@ -2,42 +2,67 @@ import './Tool.scss'
 import Talk from "../../components/Talk/Index"
 import * as conversation from "../../content/conversation.json"
 
+const renderShelf = (name) => {
+
+    let logoPath = "asset/" + name + ".svg";
+    let altText = name; 
+    return (
+        <img src={logoPath} className="tool__shelf__icon" alt={altText} />
+    );
+}
+
 const Tools = () => {
+
+    const Languages = ['c', 'js', 'solidity', 'python'];
+    const Framework = ['react', 'expressjs', 'nodejs', 'jquery', 'bootstrap'];
+    const Tools = ['vscode', 'hyper', 'git', 'remix', 'github'];
+    const Workflow = ['travis', 'netlify', 'eslint', 'prettier'];
+    const Databases = ['mongodb', 'mysql'];
+
     return (
         <div>
             <Talk message={conversation.converation[0].tools} />
             <div className="tools__page">
                 <div className="tool__container">
-                    <div className='tool__shelf'>
+                <div className='tool__shelf'>
                         <div className="tool__shelf__label">
-                            Stack
+                            Languages
                         </div>
-                        <img src="tech-icons/react.svg" className="tool__shelf__icon" alt="tech" />
-                        <img src="tech-icons/react.svg" className="tool__shelf__icon" alt="tech" />
-                        <img src="tech-icons/react.svg" className="tool__shelf__icon" alt="tech" />
-                        <img src="tech-icons/react.svg" className="tool__shelf__icon" alt="tech" />
-                        <img src="tech-icons/react.svg" className="tool__shelf__icon" alt="tech" />
-                        <img src="tech-icons/react.svg" className="tool__shelf__icon" alt="tech" />
+                        {
+                            Languages.map((data, index) => renderShelf(data))
+                        }
                      </div>
-                    <div className='tool__shelf'>
+                     <div className='tool__shelf'>
+                        <div className="tool__shelf__label">
+                            Framework
+                        </div>
+                        {
+                            Framework.map((data, index) => renderShelf(data))
+                        }
+                     </div>
+                     <div className='tool__shelf'>
                     <div className="tool__shelf__label">
                             Tools
                         </div>
-                        <img src="tech-icons/visual-studio-code.svg" className="tool__shelf__icon" alt="tech" />
-                        <img src="tech-icons/visual-studio-code.svg" className="tool__shelf__icon" alt="tech" />
-                        <img src="tech-icons/visual-studio-code.svg" className="tool__shelf__icon" alt="tech" />
-                        <img src="tech-icons/visual-studio-code.svg" className="tool__shelf__icon" alt="tech" />
-                        <img src="tech-icons/visual-studio-code.svg" className="tool__shelf__icon" alt="tech" />
+                        {
+                            Tools.map((data, index) => renderShelf(data))
+                        }
                     </div>
                     <div className='tool__shelf'>
                     <div className="tool__shelf__label">
                             Workflow
                         </div>
-                        <img src="tech-icons/hyper.svg" className="tool__shelf__icon" alt="tech" />
-                        <img src="tech-icons/hyper.svg" className="tool__shelf__icon" alt="tech" />
-                        <img src="tech-icons/hyper.svg" className="tool__shelf__icon" alt="tech" />
-                        <img src="tech-icons/hyper.svg" className="tool__shelf__icon" alt="tech" />
-                        <img src="tech-icons/hyper.svg" className="tool__shelf__icon" alt="tech" />
+                        {
+                            Workflow.map((data, index) => renderShelf(data))
+                        }
+                    </div>
+                    <div className='tool__shelf'>
+                    <div className="tool__shelf__label">
+                            Databases
+                        </div>
+                        {
+                            Databases.map((data, index) => renderShelf(data))
+                        }
                     </div>
                 </div>
                 <div className="soothing">
